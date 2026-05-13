b38afe1e02198087089012e55d57df7c726a89ba
Artur <artur_grishin_12345@mail.ru>
2026-05-13 19:58:15 +0700

    Add SQLite files, Message and User classes

---
+++ BobrNET/common/User.hpp
@@ -0,0 +1,16 @@
+class User {
+public:
+	User() = default;
+	User(int id, const std::string& login, const std::string& password_hash)
+		: m_id(id), m_login(login), m_password_hash(password_hash) {
+	}
+
+	int id() const { return m_id; }
+	const std::string& login() const { return m_login; }
+	const std::string& password_hash() const { return m_password_hash; }
+
+private:
+	int m_id = -1;
+	std::string m_login;
+	std::string m_password_hash;
+};
