2eae0f7086a7fe08f56ceaabfac297dc033209ec
Artur <artur_grishin_12345@mail.ru>
2026-05-23 12:45:39 +0700

    Add icon

--- BobrNET/client/gui/main.cpp
+++ BobrNET/client/gui/main.cpp
@@ -4,6 +4,9 @@
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
+
+    app.setWindowIcon(QIcon("icon.ico"));
+
     MainWindow window;
     window.show();
     return app.exec();
