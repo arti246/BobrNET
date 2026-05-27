6ad896d0e79c140012f6627a65fc4d3e96c029dd
Artur <artur_grishin_12345@mail.ru>
2026-05-27 10:00:58 +0700

    Add picture on LoginDialog

--- BobrNET/client/gui/LoginDialog.cpp
+++ BobrNET/client/gui/LoginDialog.cpp
@@ -1,40 +1,57 @@
 #include "LoginDialog.h"
+#include <qmainwindow.h>
 #include <QVBoxLayout>
 #include <QHBoxLayout>
 
 LoginDialog::LoginDialog(QWidget* parent) : QDialog(parent), m_isRegister(false)
 {
-    setWindowTitle("BobrNET - Login");
+    setWindowTitle("BobrNET - Авторизация");
     setMinimumSize(350, 300);
     setWindowIcon(QIcon(":/icon.ico"));
 
-    QVBoxLayout* layout = new QVBoxLayout(this);
+    // Главный горизонтальный layout
+    QHBoxLayout* mainLayout = new QHBoxLayout(this);
+    mainLayout->setSpacing(30);
 
-    // Поле Логин
-    layout->addWidget(new QLabel("Login:"));
+    // Левый layout
+    QVBoxLayout* leftLayout = new QVBoxLayout;
+    leftLayout->addWidget(new QLabel("Логин:"));
     m_loginEdit = new QLineEdit;
-    layout->addWidget(m_loginEdit);
+    leftLayout->addWidget(m_loginEdit);
 
-    // Поле Пароль
-    layout->addWidget(new QLabel("Password:"));
+    leftLayout->addWidget(new QLabel("Пароль:"));
     m_passwordEdit = new QLineEdit;
     m_passwordEdit->setEchoMode(QLineEdit::Password);
-    layout->addWidget(m_passwordEdit);
+    leftLayout->addWidget(m_passwordEdit);
 
-    // Поле Дата рождения (только для регистрации, но показываем всегда)
-    layout->addWidget(new QLabel("Birthday (YYYY-MM-DD):"));
+    leftLayout->addWidget(new QLabel("Дата рождения (YYYY-MM-DD):"));
     m_birthdayEdit = new QLineEdit;
     m_birthdayEdit->setPlaceholderText("YYYY-MM-DD");
-    layout->addWidget(m_birthdayEdit);
+    leftLayout->addWidget(m_birthdayEdit);
 
     // Кнопки
-    m_loginBtn = new QPushButton("Login");
-    m_registerBtn = new QPushButton("Register");
+    m_loginBtn = new QPushButton("Авторизация");
+    m_registerBtn = new QPushButton("Регистрация");
 
     QHBoxLayout* btnLayout = new QHBoxLayout;
     btnLayout->addWidget(m_loginBtn);
     btnLayout->addWidget(m_registerBtn);
-    layout->addLayout(btnLayout);
+    leftLayout->addLayout(btnLayout);
+
+    // Правый layout
+    QVBoxLayout* rightLayout = new QVBoxLayout;
+    QLabel* label_img = new QLabel;
+    QPixmap pixmap(":/bobr_han.jpg");
+    label_img->setPixmap(pixmap);
+    label_img->setScaledContents(true);
+    label_img->setFixedSize(230, 300);
+    rightLayout->addStretch();
+    rightLayout->addWidget(label_img);
+    rightLayout->addStretch();
+
+    // Оба layout в главный
+    mainLayout->addLayout(leftLayout);
+    mainLayout->addLayout(rightLayout);
 
     // Подключаем сигналы
     connect(m_loginBtn, &QPushButton::clicked, this, &LoginDialog::onLoginClicked);
